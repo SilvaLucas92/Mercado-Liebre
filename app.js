f854912293348e5aa3e45efae7301e3c28520c4f
SilvaLucas92 <l.silva8692@gmail.com>
2021-11-29 20:06:34 -0300

    header listo

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+
+app.listen(3000,()=>{
+    console.log("Servidor corriendo en el puerto 3000")
+});
+
+app.use(express.static(path.resolve(__dirname, "public")))
+
+app.get("/", (req, res)=>{
+    let htmlPath = path.resolve(__dirname, "views/home.html")
+    res.sendFile(htmlPath);
+});
