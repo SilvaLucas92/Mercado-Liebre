94586dee9ccb64a312c05ce655d3a7bea305273e
SilvaLucas92 <l.silva8692@gmail.com>
2021-12-13 21:47:02 -0300

    cambio puerto

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require("path");
 
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000 ,()=>{
     console.log("Servidor corriendo en el puerto 3000")
 });
 
